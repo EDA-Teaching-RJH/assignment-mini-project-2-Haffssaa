90bdd140fd32dc05e5d5e79ba020835b6a513fae
Haffssaa <hm573@kent.ac.uk>
2024-12-10 23:56:33 +0000

    d

---
+++ stringConverter.py
@@ -0,0 +1,6 @@
+# stringconverter.py
+def string_length(s): # Function to get the length of a string
+    return len(s)  # Returns the length of the string
+
+def to_uppercase(s): # Function to convert string to uppercase
+    return s.upper()  # Converts the string to uppercase
